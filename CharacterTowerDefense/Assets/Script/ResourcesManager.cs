78e2b05b93ebecbf316042c8c8db790417389352
KJB <kjb_jh@naver.com>
2025-01-27 15:04:06 +0900

    Add ResourcesInstance Property
    # ResourcesInstance 프로퍼티 추가.

--- CharacterTowerDefense/Assets/Script/ResourcesManager.cs
+++ CharacterTowerDefense/Assets/Script/ResourcesManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ResourcesManager : MonoBehaviour
 {
+    static ResourcesManager _instance;
+
     public Object Load(string _path)
     {
         return Resources.Load(_path);
@@ -10,5 +12,22 @@ public class ResourcesManager : MonoBehaviour
     public T Load<T>(string _path) where T : Object
     {
         return Resources.Load<T>(_path);
+    }
+
+    public static ResourcesManager GetResourcesInstance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject();
+                go.name = "ResourceManager";
+                go.AddComponent<ResourcesManager>();
+
+                _instance = go.GetComponent<ResourcesManager>();
+            }
+
+            return _instance;
+        }
     }
 }
