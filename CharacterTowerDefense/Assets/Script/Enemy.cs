04baf1b18522d624d19aa462a113862c74dc5b47
KJB <kjb_jh@naver.com>
2025-02-05 11:23:09 +0900

    Enemy movement function added.
    # Enemy 이동 기능 추가.

--- CharacterTowerDefense/Assets/Script/Enemy.cs
+++ CharacterTowerDefense/Assets/Script/Enemy.cs
@@ -1,8 +1,14 @@
 using NUnit.Framework;
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Movement2D movement2D = null;
+    private int WaypointCount = 0;
+    private int CurrentIndex = 0;
+    private int RotateSpeed = 10;
+
     void Start()
     {        
         // Component Setting
@@ -19,5 +25,50 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void EnemySetting()
+    {
+        movement2D = GetComponent<Movement2D>();
+
+        WaypointCount = ObjectManager.Instance.GetWayPoints().Count;
+
+        this.transform.position = ObjectManager.Instance.GetWayPoints()[WaypointCount].transform.position;
+
+        StartCoroutine("OnMove");
+    }
+
+    private IEnumerator OnMove()
+    {
+        NextMoveDirection();
+
+        while (true)
+        {
+            // È¸Àü.
+            this.transform.Rotate(Vector3.forward * RotateSpeed);
+
+            if(Vector3.Distance(this.transform.position, ObjectManager.Instance.GetWayPoints()[WaypointCount].transform.position) < 0.02f * movement2D.GetMoveSpeed())
+            {
+                NextMoveDirection();
+            }
+
+            yield return null;
+        }
+    }
+
+    private void NextMoveDirection()
+    {
+        if (CurrentIndex < WaypointCount)
+        {
+            this.transform.position = ObjectManager.Instance.GetWayPoints()[CurrentIndex].transform.position;
+            CurrentIndex++;
+
+            Vector3 Dir = (ObjectManager.Instance.GetWayPoints()[CurrentIndex].transform.position - this.transform.position).normalized;
+            movement2D.SetMoveDir(Dir);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
