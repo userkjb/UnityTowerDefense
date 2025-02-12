68e02afe211265c2423da6d76941dec89089228d
KJB <kjb_jh@naver.com>
2025-02-12 13:47:06 +0900

    Add and write Script to manage PlayerHP.
    # PlayerHP를 관리하는 Script 추가 및 작성.

---
+++ CharacterTowerDefense/Assets/Script/PlayerHP.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+    [SerializeField]
+    private float MaxHP = 20.0f;
+    private float CurrentHP = 0.0f;
+
+
+    private void Awake()
+    {
+        CurrentHP = MaxHP;
+    }
+
+    public void TakeDamage(float _Damage)
+    {
+        CurrentHP -= _Damage;
+
+        if(CurrentHP <= 0.0f)
+        {
+            Debug.Log("ÃßÈÄ ÄÁÅÙÃ÷ Ãß°¡.");
+        }
+    }
+
+    public float GetPlayerHP()
+    {
+        return CurrentHP;
+    }
+}
